249712e4e355b5dc3a45e9d3bdfcb7d0ba073c73
kekailei <2513084541@qq.com>
2018-06-10 18:21:20 +0800

    图算法自己实现62018年6月10

---
+++ graphDS/testGraph.cpp
@@ -0,0 +1,32 @@
+
+#include "stdafx.h"
+#include "testGraph.h"
+
+
+
+int main()
+{
+	Graph<const char*, int> graph(7);
+	const char* vertex[7] = { "地大", "武大", "华科 ", 
+		"交大 ", "北大 ", "清华 ", "复旦" };
+	for (size_t i = 0; i < 7; i++)
+	{
+		graph.insertVertex(vertex[i]);
+	}
+	cout << "----->图的初始化：======================================\n";
+	graph.printGraph();
+
+	cout << "图中的顶点数目：" << graph.getNumVertex() << endl;
+
+	int edge[7][3] = { { 0, 1, 43 }/*地大到武大的距离*/,{ 0, 2, 12 },{ 1, 2, 38 },{ 2, 3 ,1325 },
+	{ 3, 6, 55 },{ 4, 5, 34 },{ 4, 6, 248 } };    //分配距离
+	for (size_t i = 0; i < 7; i++)
+	{
+		graph.insertEdge(edge[i][0], edge[i][1], edge[i][2]);
+		graph.insertEdge(edge[i][1], edge[i][0], edge[i][2]);
+	}
+	cout << "添加边之后的图结构：============" << endl;
+	graph.printGraph();
+	system("color  0A");
+	return 1;
+}
---
+++ graphDS/testGraph.h
@@ -0,0 +1,187 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+template <typename DistType>
+class Edge
+{
+public:
+	Edge(int index, DistType weight)
+	{
+		m_posTable = index;
+		m_weight = weight;
+		m_next = nullptr;
+	}
+	~Edge()
+	{
+
+	}
+public:
+	int m_posTable;//改变的目的顶点所在顶点集中的位置
+	DistType m_weight;//边的的权值
+	Edge<DistType>* m_next;//下一条边
+};
+
+
+
+template <typename NameType, typename DistType> class Graph;
+template <typename NameType, typename DistType>
+class Vertex
+{
+public:
+	Vertex()
+	{
+		m_adjcentEdge = nullptr;
+		m_vertexName = 0;
+	}
+	~Vertex()
+	{
+		Edge<DistType> *pmove =this->m_adjcentEdge;
+		while (pmove)
+		{
+			m_adjcentEdge = pmove->m_next;
+			delete pmove;
+			pmove = m_adjcentEdge;
+		}
+	}
+private:
+	friend class Graph<NameType, DistType>;
+	NameType m_vertexName;
+	Edge<DistType>* m_adjcentEdge;//顶点的邻边
+};
+
+
+
+template <typename NameType, typename DistType>
+class Graph
+{
+public:
+	Graph(int size = m_defaultSize )
+	{
+		m_VertxTable = new Vertex<NameType, DistType>[size];
+		if (m_VertxTable == nullptr)
+		{
+			exit(1);
+		}
+		m_numVertexs = 0;
+		m_numEdges = 0;
+		m_maxSize = size;
+	}
+	~Graph()
+	{
+		Edge<DistType> *pmove;
+		for (size_t i = 0; i < this->m_numVertexs; i++)
+		{
+			pmove = this->m_VertxTable[i].m_adjcentEdge;
+			if (pmove)
+			{
+				this->m_VertxTable[i].m_adjcentEdge = pmove->m_next;
+				delete pmove;
+				pmove = this->m_VertxTable[i].m_adjcentEdge;
+			}
+
+		}
+		delete[] m_VertxTable;
+	}
+	int getNumVertex()
+	{
+		return this->m_numVertexs;
+	}
+	int getNumEdges()
+	{
+		return this->m_numEdges / 2;
+	}
+	bool isGraphFull()
+	{
+		return m_numVertexs == m_maxSize;
+	}
+	//插入顶点名字为vertex的顶点
+	bool insertVertex(const NameType vertexName)
+	{
+		if (isGraphFull())
+		{
+			cerr << "图已经满了， 请勿再插入顶点！\n";
+			return false;
+		}
+		else
+		{
+			this->m_VertxTable[this->m_numVertexs].m_vertexName = vertexName;
+			this->m_numVertexs++;
+		}
+		return true;
+	}
+
+	//在顶点中位置为p1和p2的顶点之间插入边
+	bool insertEdge(int p1, int p2, DistType weight = m_infinite)
+	{
+		if (p1 <0 && p2 > this->m_numVertexs && p2 <0 && p2>this->m_numVertexs)
+		{
+			cerr << "边的位置参数设置错误， 请检查！\n";
+			return false;
+		}
+		else
+		{
+			Edge<DistType>* pmove = m_VertxTable[p1].m_adjcentEdge;
+			if (pmove == nullptr)//如果顶点p1没有邻边
+			{
+				//将p2接入p1的后面
+				m_VertxTable[p1].m_adjcentEdge = new Edge<DistType>(p2, weight);
+				m_numEdges++;
+				return true;
+			}
+			else
+			{
+				while (pmove->m_next)
+				{
+					pmove = pmove->m_next;
+				}
+				pmove->m_next = new Edge<DistType>(p2, weight);
+				m_numEdges++;
+				return true;
+			}
+		}
+	}
+	//打印图中各个顶点及其链接边的权重
+	void printGraph()
+	{
+		Edge<DistType> *pmove;
+		for (size_t i = 0; i < this->m_numVertexs; i++)
+		{
+			cout << this->m_VertxTable[i].m_vertexName << "---->";
+			pmove = this->m_VertxTable[i].m_adjcentEdge;
+			while (pmove)
+			{
+				cout << pmove->m_weight << "---->" << this->m_VertxTable[pmove->m_posTable].m_vertexName << "---->";
+				pmove = pmove->m_next;
+			}
+			cout << "顶点所邻接的边已经输入完毕；\n";
+		}
+	}
+private:
+	Vertex<NameType, DistType> *m_VertxTable;//顶点集
+	int m_numVertexs;//当前顶点的数量
+	int m_maxSize;
+	const static int m_defaultSize = 10;
+	const static DistType m_infinite = 65536;//边默认最大权值
+	int m_numEdges;//图中边的数量
+
+	//用该顶点的名字来寻找其在顶点集中的位置
+	int getVertexPostable(const NameType vertexName)
+	{
+		for (size_t i = 0; i < this->m_numVertexs; i++)
+		{
+			if (vertexName == this->m_VertxTable[i].m_vertexName)
+			{
+				return i;
+			}
+			
+		}
+		return -1;
+	}
+};
+
+
+
+	
+
